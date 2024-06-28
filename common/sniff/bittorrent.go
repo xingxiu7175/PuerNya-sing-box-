Drake-welt/sing-box

concurrent sniffers

common/sniff/bittorrent.go
@@ -6,6 +6,7 @@ import (
 	"encoding/binary"
 	"io"
 	"os"
+	"sync"
 
 	"github.com/sagernet/sing-box/adapter"
 	C "github.com/sagernet/sing-box/constant"
@@ -25,45 +26,61 @@ const (
 
 // BitTorrent detects if the stream is a BitTorrent connection.
 // For the BitTorrent protocol specification, see https://www.bittorrent.org/beps/bep_0003.html
-func BitTorrent(_ context.Context, reader io.Reader) (*adapter.InboundContext, error) {
+func BitTorrent(_ context.Context, reader io.Reader, sniffdata chan SniffData, wg *sync.WaitGroup) {
+	var data SniffData
+	defer func() {
+		sniffdata <- data
+		wg.Done()
+	}()
+
 	var first byte
 	err := binary.Read(reader, binary.BigEndian, &first)
 	if err != nil {
-		return nil, err
+		data.err = err
+		return
 	}
 
 	if first != 19 {
-		return nil, os.ErrInvalid
+		data.err = os.ErrInvalid
+		return
 	}
 
 	var protocol [19]byte
 	_, err = reader.Read(protocol[:])
 	if err != nil {
-		return nil, err
+		data.err = err
+		return
 	}
 	if string(protocol[:]) != "BitTorrent protocol" {
-		return nil, os.ErrInvalid
+		data.err = os.ErrInvalid
+		return
 	}
 
-	return &adapter.InboundContext{
-		Protocol: C.ProtocolBitTorrent,
-	}, nil
+	data.metadata = &adapter.InboundContext{Protocol: C.ProtocolBitTorrent}
 }
 
 // UTP detects if the packet is a uTP connection packet.
 // For the uTP protocol specification, see
 //  1. https://www.bittorrent.org/beps/bep_0029.html
 //  2. https://github.com/bittorrent/libutp/blob/2b364cbb0650bdab64a5de2abb4518f9f228ec44/utp_internal.cpp#L112
-func UTP(_ context.Context, packet []byte) (*adapter.InboundContext, error) {
+func UTP(_ context.Context, packet []byte, sniffdata chan SniffData, wg *sync.WaitGroup) {
+	var data SniffData
+	defer func() {
+		sniffdata <- data
+		wg.Done()
+	}()
+
 	// A valid uTP packet must be at least 20 bytes long.
 	if len(packet) < 20 {
-		return nil, os.ErrInvalid
+		data.err = os.ErrInvalid
+		return
 	}
 
 	version := packet[0] & 0x0F
 	ty := packet[0] >> 4
 	if version != 1 || ty > 4 {
-		return nil, os.ErrInvalid
+		data.err = os.ErrInvalid
+		return
 	}
 
 	// Validate the extensions
@@ -72,28 +89,35 @@ func UTP(_ context.Context, packet []byte) (*adapter.InboundContext, error) {
 	for extension != 0 {
 		err := binary.Read(reader, binary.BigEndian, &extension)
 		if err != nil {
-			return nil, err
+			data.err = err
+			return
 		}
 
 		var length byte
 		err = binary.Read(reader, binary.BigEndian, &length)
 		if err != nil {
-			return nil, err
+			data.err = err
+			return
 		}
 		_, err = reader.Seek(int64(length), io.SeekCurrent)
 		if err != nil {
-			return nil, err
+			data.err = err
+			return
 		}
 	}
 
-	return &adapter.InboundContext{
-		Protocol: C.ProtocolBitTorrent,
-	}, nil
+	data.metadata = &adapter.InboundContext{Protocol: C.ProtocolBitTorrent}
 }
 
 // UDPTracker detects if the packet is a UDP Tracker Protocol packet.
 // For the UDP Tracker Protocol specification, see https://www.bittorrent.org/beps/bep_0015.html
-func UDPTracker(_ context.Context, packet []byte) (*adapter.InboundContext, error) {
+func UDPTracker(_ context.Context, packet []byte, sniffdata chan SniffData, wg *sync.WaitGroup) {
+	var data SniffData
+	defer func() {
+		sniffdata <- data
+		wg.Done()
+	}()
+
 	switch {
 	case len(packet) >= trackerConnectMinSize &&
 		binary.BigEndian.Uint64(packet[:8]) == trackerProtocolID &&
@@ -104,10 +128,10 @@ func UDPTracker(_ context.Context, packet []byte) (*adapter.InboundContext, erro
 		fallthrough
 	case len(packet) >= trackerScrapeMinSize &&
 		binary.BigEndian.Uint32(packet[8:12]) == trackerScrapeFlag:
-		return &adapter.InboundContext{
-			Protocol: C.ProtocolBitTorrent,
-		}, nil
+		data.metadata = &adapter.InboundContext{Protocol: C.ProtocolBitTorrent}
+		return
 	default:
-		return nil, os.ErrInvalid
+		data.err = os.ErrInvalid
+		return
 	}
 }

xingxiu7175/PuerNya-sing-box-

common/sniff/bittorrent.go
@@ -7,30 +7,47 @@ import (
 	"io"
 	"math"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/sagernet/sing-box/adapter"
 	C "github.com/sagernet/sing-box/constant"
 )
 
-func BittorrentTCPMessage(ctx context.Context, reader io.Reader) (*adapter.InboundContext, error) {
+func BittorrentTCPMessage(ctx context.Context, reader io.Reader, sniffdata chan SniffData, wg *sync.WaitGroup) {
+	var data SniffData
+	defer func() {
+		sniffdata <- data
+		wg.Done()
+	}()
+
 	packet, err := io.ReadAll(reader)
 	if err != nil {
-		return nil, os.ErrInvalid
+		data.err = os.ErrInvalid
+		return
 	}
 	if len(packet) < 20 {
-		return nil, os.ErrInvalid
+		data.err = os.ErrInvalid
+		return
 	}
 	if packet[0] != 19 || string(packet[1:20]) != "BitTorrent protocol" {
-		return nil, os.ErrInvalid
+		data.err = os.ErrInvalid
+		return
 	}
-	return &adapter.InboundContext{Protocol: C.ProtocolBittorrent}, nil
+	data.metadata = &adapter.InboundContext{Protocol: C.ProtocolBittorrent}
 }
 
-func BittorrentUDPMessage(ctx context.Context, packet []byte) (*adapter.InboundContext, error) {
+func BittorrentUDPMessage(ctx context.Context, packet []byte, sniffdata chan SniffData, wg *sync.WaitGroup) {
+	var data SniffData
+	defer func() {
+		sniffdata <- data
+		wg.Done()
+	}()
+
 	pLen := len(packet)
 	if pLen < 20 {
-		return nil, os.ErrInvalid
+		data.err = os.ErrInvalid
+		return
 	}
 
 	buffer := bytes.NewReader(packet)
@@ -38,48 +55,59 @@ func BittorrentUDPMessage(ctx context.Context, packet []byte) (*adapter.InboundC
 	var typeAndVersion uint8
 
 	if binary.Read(buffer, binary.BigEndian, &typeAndVersion) != nil {
-		return nil, os.ErrInvalid
+		data.err = os.ErrInvalid
+		return
 	} else if packet[0]>>4&0xF > 4 || packet[0]&0xF != 1 {
-		return nil, os.ErrInvalid
+		data.err = os.ErrInvalid
+		return
 	}
 
 	var extension uint8
 
 	if binary.Read(buffer, binary.BigEndian, &extension) != nil {
-		return nil, os.ErrInvalid
+		data.err = os.ErrInvalid
+		return
 	} else if extension != 0 && extension != 1 {
-		return nil, os.ErrInvalid
+		data.err = os.ErrInvalid
+		return
 	}
 
 	for extension != 0 {
 		if extension != 1 {
-			return nil, os.ErrInvalid
+			data.err = os.ErrInvalid
+			return
 		}
 		if binary.Read(buffer, binary.BigEndian, &extension) != nil {
-			return nil, os.ErrInvalid
+			data.err = os.ErrInvalid
+			return
 		}
 
 		var length uint8
 
 		if err := binary.Read(buffer, binary.BigEndian, &length); err != nil {
-			return nil, os.ErrInvalid
+			data.err = os.ErrInvalid
+			return
 		}
 		if int32(pLen) >= int32(length) {
-			return nil, os.ErrInvalid
+			data.err = os.ErrInvalid
+			return
 		}
 	}
 
 	if int32(pLen) >= int32(2) {
-		return nil, os.ErrInvalid
+		data.err = os.ErrInvalid
+		return
 	}
 
 	var timestamp uint32
 
 	if err := binary.Read(buffer, binary.BigEndian, &timestamp); err != nil {
-		return nil, os.ErrInvalid
+		data.err = os.ErrInvalid
+		return
 	}
 	if math.Abs(float64(time.Now().UnixMicro()-int64(timestamp))) > float64(24*time.Hour) {
-		return nil, os.ErrInvalid
+		data.err = os.ErrInvalid
+		return
 	}
-	return &adapter.InboundContext{Protocol: C.ProtocolBittorrent}, nil
+	data.metadata = &adapter.InboundContext{Protocol: C.ProtocolBittorrent}
 }